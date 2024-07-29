e05c17cf622222bc610afa4b88cab9caee9a80fb
Ashwin <ashwinnema06@gmail.com>
2024-07-29 11:10:05 +0530

    temp

--- lib/hooks.ts
+++ lib/hooks.ts
@@ -3,4 +3,5 @@ import { AppDispatch, AppStore, RootState } from "./store";
 
 export const useAppDispatch = useDispatch.withTypes<AppDispatch>();
 export const useAppSelector = useSelector.withTypes<RootState>();
-export const useAppStore = useStore.withTypes<AppStore>();
+
+
